2f78ce18c081485dd7956aabbc5a2c9420c32393
gulshan-mittal <gulshankumar1210iiit@gmail.com>
2019-11-24 22:53:52 +0530

    Added global header file

---
+++ global.hpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <string>
+#include <fstream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/core.hpp"
+#include <time.h>
+#include "iostream"
+using namespace std;
+using namespace cv;
